fa1220c5a098c8bf4df81a5d1991fd7c1b153665
Stein Arild <103763064+Arkuradev@users.noreply.github.com>
2025-06-17 14:53:29 +0200

    Set up Tailwind cli

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: [
+    "./index.html",
+    "./auth/**/*.html",
+    "./user/**/*.html",
+    "./packlineMalbok/**/*.html",
+    "./js/**/*.mjs",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
